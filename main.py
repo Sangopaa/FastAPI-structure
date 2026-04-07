c42d180a9aaccde9382b7019606718eee5429546
Sangopaa <sangopaa@gmail.com>
2026-04-06 21:13:18 -0500

    Empty project

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from routers.auth import auth_router
 
 app = FastAPI()
-
-app.include_router(auth_router)
 
 
 @app.get("/")
--- models/auth/user.py
+++
@@ -1,6 +0,0 @@
-from models.base_model import BaseModel
-
-
-class User(BaseModel):
-    email: str
-    password: str
