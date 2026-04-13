64c001c1aabf20154446d963be4650dbe45bb983
Sangopaa <sangopaa@gmail.com>
2026-04-12 23:20:25 -0500

    Adding CORS

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.v1.api_router import v1_router
 
 app = FastAPI(title="FastAPI Structure")
+
+# Configuración de CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes en desarrollo. Modificar para producción.
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los headers
+)
 
 app.include_router(v1_router, prefix="/api/v1")
 
